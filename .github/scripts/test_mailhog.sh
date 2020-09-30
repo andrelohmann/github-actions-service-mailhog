79eef950f3f261d9eb1f0969113bd487bd59049f
andrelohmann <lohmann.andre@gmail.com>
2020-09-30 09:37:57 +0200

    debug mailhog

--- .github/scripts/test_mailhog.sh
+++ .github/scripts/test_mailhog.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-telnet localhost 1025 << EOF
-HELO cloudflare.com
-MAIL FROM:<test@test.com>
-RCPT TO:<test2@test.com>
-DATA
-Body of email.
-.
-QUIT
-EOF
-if [ $? -eq 0 ]
-then
-  echo "Telnet command returned 0"
-else
-  exit 0
-fi
+# create message
+function mail_input {
+echo "ehlo mailog.lokal"
+echo "MAIL FROM: <test@test.com>"
+echo "RCPT TO: <test2@test.com>"
+echo "DATA"
+echo "From: <test@test.com>"
+echo "To: <test2@test.com>"
+echo "Subject: Testing SMTP Mail"
+echo "This is only a test. Please do not panic. If this works, then all is well, else all is not well."
+echo "."
+echo "quit"
+}
+
+mail_input | netcat $server $port || err_exit
